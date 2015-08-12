153d3eec8998ed509da0f7a3219b60f4c282a785
sysucjl <cjl091454@gmail.com>
2015-08-12 17:05:49 +0800

    how to use template

--- app/views.py
+++ app/views.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python
 # coding=utf-8
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello,World!"
+    user = {'nickname':'sysucjl'}
+    posts = [ # fake array of posts
+        {
+            'author': { 'nickname': 'John' },
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': { 'nickname': 'Susan' },
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html',title='Home',user=user,posts=posts)
