c5685be682011e4702ee24799d5d409f50f0ec0a
sysucjl <cjl091454@gmail.com>
2015-08-12 17:29:20 +0800

    how to use web form

--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # coding=utf-8
 from app import app
-from flask import render_template
+from flask import render_template,flash,redirect
+from .forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -18,3 +19,16 @@ def index():
         }
     ]
     return render_template('index.html',title='Home',user=user,posts=posts)
+
+# index view function suppressed for brevity
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' + str(form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html',
+        title = 'Sign In',
+        form = form,
+        providers = app.config['OPENID_PROVIDERS'])
